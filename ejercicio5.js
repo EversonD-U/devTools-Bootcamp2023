d4f75526fb7cc74222d1da937d218f679e70a06e
4Ever <everywhere315@gmail.com>
2023-05-23 17:18:37 -0500

    Correcciones y añadir ejercicios

---
+++ ejercicio5.js
@@ -0,0 +1,14 @@
+
+
+function devolverPrimerElemento(array) {
+  // Retornar el primer elemento del arreglo recibido por parámetro.
+  // Tu código:
+  return array[0];
+}
+
+
+
+console.log(funcion1(2))
+
+
+var arreglo[10,20,30,40]
